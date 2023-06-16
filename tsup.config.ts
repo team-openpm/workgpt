e9487720a0c8f6315d194073bec290d38ec8a57d
Alex MacCaw <alex@alexmaccaw.com>
2023-06-16 10:00:19 -0700

    Expose entries

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/apis/*', 'src/chat-agents/*', 'src/runners/*'],
+  entry: [
+    'src/index.ts',
+    'src/apis/*',
+    'src/chat-agents/*',
+    'src/runners/*',
+    'src/apis/openpm',
+    'src/chat-agents/open-ai',
+    'src/runners/workgpt',
+  ],
   format: ['esm', 'cjs'],
   clean: true,
   dts: true,
