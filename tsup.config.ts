10b5248a03ea2bb816db94087267070ffab61b56
Alex MacCaw <alex@alexmaccaw.com>
2023-06-16 13:14:06 -0700

    lint

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: [
-    "src/index.ts",
+    'src/index.ts',
     'src/apis/*',
     'src/chat-agents/*',
     'src/runners/*',
