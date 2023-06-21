8b907ba8d342059b28a46b62c13bcd8a8806e2b2
Alex MacCaw <alex@alexmaccaw.com>
2023-06-21 08:10:58 -0400

    Add sitemaps to the browser.

--- src/apis/text-browser.ts
+++ src/apis/text-browser.ts
@@ -5,18 +5,73 @@ import puppeteer, { Page } from 'puppeteer'
 
 export class TextBrowser extends Api {
   @invokable({
-    usage: `Useful for getting text contents of a website.`,
+    usage: `Useful for getting plain text contents of a website.`,
     schema: z.object({
       url: z.string(),
     }),
   })
-  async browse({ url }: { url: string }): Promise<string> {
+  async getSiteText({ url }: { url: string }): Promise<string> {
     const browser = await puppeteer.launch({ headless: 'new' })
     const page = await browser.newPage()
     await page.goto(url)
     const text = await extractText(page)
     await browser.close()
     return text ?? ''
+  }
+
+  @invokable({
+    usage: `Get sitemap of a website. Useful for getting a list of pages of a website.`,
+    schema: z.object({
+      url: z.string(),
+    }),
+  })
+  async getSitemap({ url }: { url: string }) {
+    const browser = await puppeteer.launch({ headless: 'new' })
+    const base = new URL(url).hostname
+    const page = await browser.newPage()
+    await page.goto(url)
+    const links = await page.evaluate(() =>
+      Array.from(document.querySelectorAll('a[href]')).map((node) => ({
+        href: node.getAttribute('href'),
+        linkText: node.textContent,
+      }))
+    )
+    await browser.close()
+
+    const normalizedLinks: {
+      linkText: string
+      linkUrl: string
+    }[] = []
+
+    for (const { href, linkText } of links) {
+      if (!href || !linkText) {
+        continue
+      }
+
+      let url: URL
+
+      try {
+        url = new URL(href)
+      } catch (e) {
+        continue
+      }
+
+      if (url.hostname !== base) {
+        continue
+      }
+
+      // Check uniqueness
+      if (normalizedLinks.some((link) => link.linkUrl === url.href)) {
+        continue
+      }
+
+      normalizedLinks.push({
+        linkText: linkText.trim(),
+        linkUrl: url.href,
+      })
+    }
+
+    return normalizedLinks
   }
 }
 
